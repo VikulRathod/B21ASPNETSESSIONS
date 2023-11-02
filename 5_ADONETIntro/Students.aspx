b8de5507879f0bfd631cc82f6d16a9add6b4452f
vikul rathod <vikul.rathod.net@gmail.com>
2023-11-02 09:46:42 +0530

    ado.net session 1

---
+++ 5_ADONETIntro/Students.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Students.aspx.cs" Inherits="_5_ADONETIntro.Students" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>Students List</h2>
+
+            <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+            <asp:Button ID="btnLoad" runat="server" Text="Search" OnClick="btnLoad_Click"/>
+            <hr />
+            <asp:GridView ID="gvStudents" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 5_ADONETIntro/Students.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace _5_ADONETIntro
+{
+    public partial class Students : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadStudents();
+            }
+        }
+
+        private void LoadStudents()
+        {
+            //string connectionString =
+            //    "server=.\\sqlexpress;database=B21AdoNetDb;integrated security=true;";
+
+            //string connectionString =
+            //    "data source=.\\sqlexpress;initial catalog=B21AdoNetDb;integrated security=true;";
+
+            //string connectionString =
+            //    "server=.\\sqlexpress;database=B21AdoNetDb;user id=user;password=123456;";
+
+            string connectionString =
+                "server=.\\sqlexpress;database=B21AdoNetDb;integrated security=true;";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            // con.ConnectionString = connectionString;
+
+            con.Open();
+
+            string cmdText =
+                "select RollNumber, Name, Gender, City from Student";
+            SqlCommand cmd = new SqlCommand(cmdText, con);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            gvStudents.DataSource = reader;
+            gvStudents.DataBind();
+
+            con.Close();
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+            string city = txtCity.Text;
+
+            string cs = "server=.\\sqlexpress;database=B21AdoNetDb;integrated security=true;";
+            SqlConnection con = new SqlConnection(cs);
+            con.Open();
+
+            // string cmdText = $"select RollNumber, Name, Gender, City from Student where City = '{city}'";
+
+            string cmdText = $"select RollNumber, Name, Gender, City from Student where City = @City";
+            SqlCommand cmd = new SqlCommand(cmdText, con);
+            cmd.Parameters.AddWithValue("@City", city);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            gvStudents.DataSource = reader;
+            gvStudents.DataBind();
+
+            con.Close();
+        }
+    }
+}
