852867ebcbc65d0365cd53b516f12e629326109d
vikul rathod <vikul.rathod.net@gmail.com>
2023-11-27 09:59:54 +0530

    entity framework session 2

---
+++ 3-EFDBFirstAppraoch-CRUD/TrainerDashboard.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TrainerDashboard.aspx.cs" Inherits="_3_EFDBFirstAppraoch_CRUD.TrainerDashboard" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Trainer Dashborad</h2>
+        <hr />
+        <table>
+            <tr>
+                <td>Trainer Id</td>
+                <td>
+                    <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+                    <asp:Button ID="btnLoad" runat="server" Text="Load Trainer" OnClick="btnLoad_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td>Name</td>
+                <td>
+                    <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Experience</td>
+                <td>
+                    <asp:TextBox ID="txtExperience" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>City</td>
+                <td>
+                    <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnCreate" runat="server" Text="Create" OnClick="btnCreate_Click"/> |
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click"/>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+                </td>
+            </tr>
+        </table>
+        <hr />
+        <asp:GridView ID="gvTrainers" runat="server"></asp:GridView>
+    </form>
+</body>
+</html>
---
+++ 3-EFDBFirstAppraoch-CRUD/TrainerDashboard.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3_EFDBFirstAppraoch_CRUD
+{
+    public partial class TrainerDashboard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadTrainers();
+            }
+        }
+
+        void LoadTrainers()
+        {
+            B21DbContext db = new B21DbContext();
+            gvTrainers.DataSource = db.Trainers.ToList();
+            gvTrainers.DataBind();
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+            int trainerId = int.Parse(txtId.Text);
+            B21DbContext db = new B21DbContext();
+
+            Trainer trainer = db.Trainers.FirstOrDefault(t => t.Id == trainerId);
+
+            txtName.Text = trainer.Name;
+            txtExperience.Text = trainer.Experience.ToString();
+            txtCity.Text = trainer.City;
+
+            lblMessage.Text = $"Trainer details with id {trainerId} loaded successfully";
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            B21DbContext db = new B21DbContext();
+
+            Trainer t = new Trainer() 
+            {
+            Name = txtName.Text,
+            Experience = int.Parse(txtExperience.Text),
+            City = txtCity.Text
+            };
+
+            db.Trainers.Add(t);
+
+            db.SaveChanges(); // to reflect changes in database
+
+            lblMessage.Text = "New trainer created successfully";
+
+            LoadTrainers(); // to refersh trainer grid
+
+            ClearForm();
+        }
+
+        void ClearForm()
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtExperience.Text = "";
+            txtCity.Text = string.Empty;
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int trainerId = int.Parse(txtId.Text);
+
+            B21DbContext db = new B21DbContext();
+            Trainer trainer = db.Trainers.FirstOrDefault(t => t.Id == trainerId);
+
+            db.Trainers.Remove(trainer);
+            db.SaveChanges();
+
+            lblMessage.Text = $"Trainer with id {trainerId} deleted successfully";
+
+            LoadTrainers(); // to refersh trainer grid
+
+            ClearForm();
+        }
+    }
+}
