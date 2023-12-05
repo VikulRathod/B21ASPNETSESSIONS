0398594ae5f4b391a27f0328d8ea9220a7b1a87b
vikul rathod <vikul.rathod.net@gmail.com>
2023-12-05 10:04:48 +0530

    mvc session 2

--- 1-MVCProjectStructure/App_Start/RouteConfig.cs
+++ 1-MVCProjectStructure/App_Start/RouteConfig.cs
@@ -13,11 +13,23 @@ namespace _1_MVCProjectStructure
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes(); // to enable attribute based routing
+
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Trainer", action = "AllTrainers", id = UrlParameter.Optional }
+                url: "{controller}/{action}/{id}", // pattern
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                }
             );
+
+            routes.MapRoute(
+                name: "SecondRoute",
+                url: "{controller}/{action}/{id}/{name}"
+                );
         }
     }
 }
--- 1-MVCProjectStructure/Controllers/TrainerController.cs
+++ 1-MVCProjectStructure/Controllers/TrainerController.cs
@@ -4,16 +4,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace _1_MVCProjectStructure.Controllers
 {
+    // [RoutePrefix("trainer")]
+    [RoutePrefix("vhaash")]
     public class TrainerController : Controller
     {
+        // vhaash
+        [Route("")]
         public string Welcome()
         {
             return "<h1>My MVC First Application</h1>";
         }
 
+        // vhaash/vishal
+       //  [Route("{name:string}")]
+        // [Route("{id:int}")]
+        public string Hello(string name)
+        {
+            return $"Good Morning, {name}";
+        }
+
+        // trainer/alltrainers
+        // trainer/dashboard
+        // vhaash/dashboard
+        [Route("dashboard")]
         public ActionResult AllTrainers()
         {
             List<Trainer> trainers = new List<Trainer>() 
@@ -26,6 +43,18 @@ namespace _1_MVCProjectStructure.Controllers
             ViewBag.Trainers = trainers;
 
             return View();
+        }
+
+        [Route("index1/{id?}")]
+        public string Index1(int? id)
+        {
+            return $"Index 1 : Url passed values : {id ?? 0}";
+        }
+
+        [Route("index2/{id?}/{name?}")]
+        public string Index2(int? id, string name)
+        {
+            return $"Index 2 : Url passed values : {id ?? 0} {name}";
         }
     }
 }
