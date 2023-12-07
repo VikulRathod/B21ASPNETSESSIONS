5669e66336c224247860b05949d3e697efc3be2e
vikul rathod <vikul.rathod.net@gmail.com>
2023-12-07 10:00:22 +0530

    mvc session 4

---
+++ 2-ViewsDemo/Controllers/TrainerController.cs
@@ -0,0 +1,31 @@
+﻿using _2_ViewsDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2_ViewsDemo.Controllers
+{
+    public class TrainerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            ViewData["name"] = "Vihaan Rathod";
+
+            Trainer t = new Trainer() { TrainerId = 1, Name = "Usha", Experience = 3};
+            ViewData["trainer"] = t;
+
+            ViewBag.Email = "vihaan@gmail.com";
+            ViewBag.newtrainer = 
+                new Trainer() { TrainerId = 2, Name = "Atul", Experience = 2 };
+
+            return View(t);
+        }
+    }
+}
---
+++ 2-ViewsDemo/Models/Trainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2_ViewsDemo.Models
+{
+    public class Trainer
+    {
+        public int TrainerId { get; set; }
+        public string Name { get; set; }
+        public int Experience { get; set; }
+    }
+}
