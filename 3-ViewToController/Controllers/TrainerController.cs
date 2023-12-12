d040b39d5a6cef0d57f244ba7e82846d6162dc0e
vikul rathod <vikul.rathod.net@gmail.com>
2023-12-12 09:57:35 +0530

    mvc session 7

---
+++ 3-ViewToController/Controllers/TrainerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _3_ViewToController.Controllers
+{
+    public class TrainerController : Controller
+    {
+        // GET: Trainer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // [HttpGet]
+        [HttpPost]
+        // [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Details(string trainerName, string Name, 
+            string Email)
+        {
+            ViewBag.message = trainerName;
+            ViewBag.name = Name;
+            ViewBag.email = Email;
+            return View();
+        }
+    }
+}
---
+++ 3-ViewToController/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3_ViewToController.MvcApplication" Language="C#" %>
