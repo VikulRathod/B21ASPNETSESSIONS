3e52f5440b8bf1d4b2adf122a54e3edca7665bde
vikul rathod <vikul.rathod.net@gmail.com>
2023-12-18 10:02:19 +0530

    mvc session 11

---
+++ 4-CRUDUsingEF/Areas/admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _4_CRUDUsingEF.Areas.admin.Controllers
+{
+    // [RouteArea("admin")]
+    public class HomeController : Controller
+    {
+        // GET: admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
