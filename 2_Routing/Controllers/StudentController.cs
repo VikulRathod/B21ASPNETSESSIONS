061c3babe241557aeb9a7a3e8be4662089703684
vikul rathod <vikul.rathod.net@gmail.com>
2024-01-10 10:58:58 +0530

    web api 2 session

---
+++ 2_Routing/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _2_Routing.Controllers
+{
+    // [RoutePrefix("api/[controller]")]
+    public class StudentController : ApiController
+    {
+        // get https://localhost:44342/api/student
+        [HttpGet]
+        public IEnumerable<string> GetNames()
+        {
+            return new List<string>() { "Vishal", "Kunal", "Ritesh" };
+        }
+
+        [HttpGet]
+        public string GetName(int id)
+        {
+            return "Kunal";
+        }
+
+        [HttpPost]
+        // public void Create([FromBody] string name)
+        // [Route("{name}?")]
+        public void Create(string name)
+        {
+            // create logic
+        }
+
+        [HttpPut]
+        public void Update([FromUri] int id, [FromBody] string name)
+        {
+
+        }
+
+        [HttpDelete]
+        public void Delete([FromUri] int id)
+        {
+
+        }
+    }
+}
