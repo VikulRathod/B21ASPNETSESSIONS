fbc549de5d867e70aef1baa4c91f1efcc1f19255
vikul rathod <vikul.rathod.net@gmail.com>
2024-01-16 10:16:06 +0530

    web api session 5

--- 2_Routing/App_Start/WebApiConfig.cs
+++ 2_Routing/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace _2_Routing
 {
@@ -10,6 +11,9 @@ namespace _2_Routing
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+
+            // enable cors
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
--- 3_APIMvcClient/Controllers/ProductController.cs
+++ 3_APIMvcClient/Controllers/ProductController.cs
@@ -36,5 +36,49 @@ namespace _3_APIMvcClient.Controllers
                 return View(products);
             }
         }
+
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            Product product = new Product();
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:44344/api/");
+
+            var response = client.GetAsync($"product/{id}").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                product = JsonConvert.DeserializeObject<Product>(json);
+
+                return View(product);
+            }
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:44344/api/");
+
+            var response = client.PostAsJsonAsync<Product>("product", product).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("Name", "Error in calling API");
+
+            return View();
+        }
     }
 }
